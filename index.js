724de061ec23b5dd54ac19ddd2bf4590effdd64d
RonG <racg550@gmail.com>
2016-11-21 17:08:13 -0600

    added script file

---
+++ index.js
@@ -0,0 +1,46 @@
+var search = $('.search');
+var result = $('.result');
+
+search.keyup(function() {
+  if (search.val() === '') {
+    result.html('');
+    exit;
+  }
+  
+  //access API
+  $.ajax({
+    url: '//en.wikipedia.org/w/api.php',
+    data: {
+      action: 'query',
+      list: 'search',
+      srsearch: search.val(),
+      format: 'json'
+    },
+    dataType: 'jsonp',
+    //create new divs containing search results:
+    success: function(x) {
+      var html = '  <!-- result -->';
+      html += '  <div class="row row-centered">';
+      html += '  <p class="p3">Results :</p>'
+      x.query.search.map(function(w) {
+
+        html += '    <div class="col-xs-10 col-centered">';
+        html += '      <a href="https://en.wikipedia.org/wiki/' + w.title + '" target="_blank">';
+        html += '        <div class="panel panel-default">';
+        html += '          <div class="panel-heading">';
+        html += '            <h3 class="panel-title">' + w.title + '</h3>';
+        html += '          </div>';
+        html += '          <div class="panel-body">';
+        html += '            ' + w.snippet;
+        html += '          </div>';
+        html += '        </div>';
+        html += '      </a>';
+        html += '    </div>';
+      });
+
+      html += '  </div>';
+
+      result.html(html);
+    }
+  });
+});
